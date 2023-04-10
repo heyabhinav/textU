05aaaf82699e0878a7797392d4c9bd97bc1ee6a4
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2023-04-10 22:21:35 +0530

    Added Remove new line

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -3,25 +3,36 @@ import React,{useState} from "react";
 function InputBox(props){
     const handleUpperCase = () => {
         setText(text.toUpperCase());
+        props.showAlert("success", "Text converted to Upper Case");
     }
 
     const handleLowerCase = () => {
         setText(text.toLowerCase());
+        props.showAlert("success", "Text converted to Lower Case");
     }
 
     const handleClear = () => {
         setText("");
+        props.showAlert("success", "Text Cleared");
     }
 
     const handleCopy = () => {
         var text = document.getElementById("inputBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("success", "Text Copied to Clipboard");
     }
 
     const handleExtraSpaces = () => {
         var newtext = text.split(/[ ]+/);
         setText(newtext.join(" "));
+        props.showAlert("success", "Extra Spaces Removed");
+    }
+    
+    const handleNewLine = () => {
+        var newtext = text.split(/[\n]+/);
+        setText(newtext.join(" "));
+        props.showAlert("success", "New Line Removed");
     }
 
     const handleChange = (event) => {
@@ -44,11 +55,13 @@ function InputBox(props){
             <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleClear}>Clear</button>
             <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy</button>
             <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            <button type="button" className="btn btn-primary mx-1 my-1" onClick={handleNewLine}>Remove New Line</button>
             <div className="container my-4">
                 <h1>Text Summary</h1>
-                <h6>Words : {text.split(" ").length}</h6>
-                <h6>Characters : {text.length}</h6>
-                <h6>Reading time : {0.008 * text.split(" ").length}</h6>
+                <p>Words : {text.split(" ").length}</p>
+                <p>Characters : {text.length}</p>
+                <p>Reading time : {0.008 * text.split(" ").length}</p>
+                <br></br>
                 <h3>Preview</h3>
                 <p>{text}</p>
             </div>
